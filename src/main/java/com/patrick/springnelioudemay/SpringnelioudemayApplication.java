b6b1db4426956c05a1833e43897942c4c04a4ea7
Patrick Moura <patrickmoura@gmail.com>
2021-04-27 19:22:48 -0300

    Iniciando o Projeto

---
+++ src/main/java/com/patrick/springnelioudemay/SpringnelioudemayApplication.java
@@ -0,0 +1,13 @@
+package com.patrick.springnelioudemay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringnelioudemayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringnelioudemayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patrick/springnelioudemay/SpringnelioudemayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrick.springnelioudemay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringnelioudemayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
